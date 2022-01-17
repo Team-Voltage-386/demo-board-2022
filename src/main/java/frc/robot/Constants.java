c596f80b6d1d40f4bc0cc2c61196e12f6ba61e26
Debbie <31257748+Debbie386@users.noreply.github.com>
2022-01-17 16:10:45 -0500

    Added constants for the controller buttons and the pneumatics

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,46 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final class ControllerConstants {
+        // Joysticks
+        public static final int kLeftVertical = 1;
+        public static final int kLeftHorizontal = 0;
+        public static final int kRightVertical = 5;
+        public static final int kRightHorizontal = 4;
+        public static final int kLeftTrigger = 2;
+        public static final int kRightTrigger = 3;
+        // Buttons
+        public static final int kA = 1;
+        public static final int kB = 2;
+        public static final int kX = 3;
+        public static final int kY = 4;
+        public static final int kLeftBumper = 5;
+        public static final int kRightBumper = 6;
+        public static final int kLeftOptions = 7;
+        public static final int kRightOptions = 8;
+        public static final int kLeftJoystickPressed = 9;
+        public static final int kRightJoystickPressed = 10;
+    }
+
+    // Pneumatics constants
+    public static final int kForwardChannel = 0; // PCM
+    public static final int kReverseChannel = 1; // PCM
+    public static final DoubleSolenoid.Value kPistonOut = DoubleSolenoid.Value.kForward;
+    public static final DoubleSolenoid.Value kPistonIn = DoubleSolenoid.Value.kReverse;
+}
