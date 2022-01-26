b13bd46a48907eff1e5d325887fc13db212f73fa
Daplin2 <dd1aa6560@gmail.com>
2022-01-25 20:45:24 -0500

    working on led light subsystem

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+// 76 lights on the strip --> set.length(76)
+public class LEDSubsystem 
+{
+    @Override
+    public void robotInit() {
+      // PWM port 2
+      // Must be a PWM header, not MXP or DIO
+      m_led = new AddressableLED(2);
+  
+      // Reuse buffer
+      // Default to a length of 60, start empty output
+      // Length is expensive to set, so only set it once, then just update data
+      m_ledBuffer = new AddressableLEDBuffer(76);
+  
+      // Set the data
+      m_led.setData(m_ledBuffer);
+      m_led.start();
+    }
+}
