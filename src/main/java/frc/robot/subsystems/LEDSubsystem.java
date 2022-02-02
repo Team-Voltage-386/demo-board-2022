55a897cf60b65beaaf3dfb7673f3ec8a4d93c4a3
Daplin2 <dd1aa6560@gmail.com>
2022-02-01 19:17:03 -0500

    led code

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.LedConstants.*;
+
+public class LEDSubsystem extends SubsystemBase {
+  /** Creates a new LEDSubsystem. */
+  public LEDSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
