534783f7b40dab5c37513f49d89e3e7d1c28b0a7
maxhv1 <max.hero.factory@gmail.com>
2022-02-11 20:20:49 -0500

    USB as CameraServer works (also gradle is good)

--- src/main/java/frc/robot/subsystems/USBCam.java
+++ src/main/java/frc/robot/subsystems/USBCam.java
@@ -6,58 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cscore.VideoSource;
-import edu.wpi.first.wpilibj.TimedRobot;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class USBCam extends SubsystemBase {
-  private Thread m_visionThread;
-  private CvSource outputStream;
-  private CvSink cvSink;
-
-  // create shuffleboard tab
-  private ShuffleboardTab tab = Shuffleboard.getTab("USB Cam");
-  // camera widget
-  private ComplexWidget camWidget = tab.add(outputStream).withPosition(1, 1).withSize(2, 2);
 
   /** Creates a new USBCam. */
   public USBCam() {
-    new Thread(() -> {
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-      camera.setResolution(640, 480);
-
-      cvSink = CameraServer.getInstance().getVideo();
-      outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-
-      Mat source = new Mat();
-      Mat output = new Mat();
-
-      while (!Thread.interrupted()) {
-        cvSink.grabFrame(source);
-        Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-        outputStream.putFrame(output);
-      }
-    }).start();
-
+    CameraServer.startAutomaticCapture();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    CameraServer.startAutomaticCapture();
-
-    camWidget.
   }
 }
