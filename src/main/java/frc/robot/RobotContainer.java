cd7866f5f9047823c685dc4ca8a9a68733fc01ee
maxhv1 <max.hero.factory@gmail.com>
2022-02-16 18:37:34 -0500

    camera sort of worked

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.USBCam;
+import frc.robot.subsystems.Vision;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -26,7 +26,7 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
-  private final USBCam m_USBCam = new USBCam();
+  private final Vision m_Vision = new Vision();
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
